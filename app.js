56374e01f09c8be9573b03da1fd0a13c5b5dec74
DeenRamah <96067114+DeenRamah@users.noreply.github.com>
2023-02-17 08:07:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+//nunez
+//liv0
+//
+const body = document.querySelector("body");
+ 
+let ras = document.getElementsByClassName(".ras");
+let logo = document.getElementsByClassName(".logo");
+let bidhaa = document.getElementsByClassName(".bidhaa");
+let list = document.getElementsByClassName(".maliLnk");
+let rada = document.getElementsByClassName(".rada");
+let mkuu = document.getElementsByClassName(".liveh liv0");
+let salah = document.getElementsByClassName(".salah");
+let nunez = document.getElementsByClassName(".nunez");
+let button = document.getElementsByClassName(".bts");
+
+
+body = document.getElementsByName("body");
+ras = document.querySelector(".ras");
+logo = document.querySelector(".logo");
+bidhaa = document.querySelector(".bidhaa");
+list = document.querySelector(".maliLnk");
+rada = document.querySelector(".rada");
+mkuu = document.querySelector(".liveh liv0");
+salah = document.querySelector(".salah");
+nunez = document.querySelector(".nunez");
+button = document.querySelector(".bts");
+
+
+
+var typingEffect = new Typed(".nunez",{
+    strings : ["coder", "Youtuber", "designer"],
+    loop : true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backdelay: 1500
+})
